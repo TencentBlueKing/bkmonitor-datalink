1762e5cc6f26cde548e31592a5d342bd5c490704
bellkeyang <bellkeyang@tencent.com>
2023-12-04 09:42:02 +0800

    feat: 添加周期任务的示例及默认配置

--- pkg/bk-monitor-worker/internal/example/example.go
+++ pkg/bk-monitor-worker/internal/example/example.go
@@ -29,7 +29,15 @@ func HandleExampleTask(ctx context.Context, t *task.Task) error {
 	if err := json.Unmarshal(t.Payload, &p); err != nil {
 		return fmt.Errorf("json.Unmarshal failed: %v", err)
 	}
+
 	//逻辑处理start...
 	logger.Printf("print user info: user_id=%d", p.UserID)
 	return errors.New("this is a test")
+}
+
+func PeriodicHandleExampleTask(ctx context.Context, t *task.Task) error {
+	logger.Info("periodic example func trigger")
+
+	logger.Printf("periodic end")
+	return nil
 }

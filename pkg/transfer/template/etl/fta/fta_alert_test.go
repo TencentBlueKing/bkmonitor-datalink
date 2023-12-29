711bf9087a503910825d3df291a7e9f16d84a609
lai <11598235+unique0lai@users.noreply.github.com>
2023-12-29 19:32:36 +0800

    test: 补充fta etl单元测试

--- pkg/transfer/template/etl/fta/fta_alert_test.go
+++ pkg/transfer/template/etl/fta/fta_alert_test.go
@@ -30,7 +30,7 @@ func (s *AlertFTATest) TestEvent() {
 		`{
 			"etl_config":"bk_fta_event",
 			"option":{
-				"fta_clean_configs": [
+				"clean_configs": [
 					{
 						"alert_config": [
 							{
@@ -225,6 +225,264 @@ func (s *AlertFTATest) TestEvent() {
 				"bk_ingest_time": 1618210322.0,
 				"event_id":       "123",
 				"plugin_id":      "bkplugin",
+			}, result)
+		},
+	)
+}
+
+func (s *AlertFTATest) TestCleanConfig() {
+	s.CTX = testsuite.PipelineConfigStringInfoContext(
+		s.CTX, s.PipelineConfig,
+		`{
+			"etl_config":"bk_fta_event",
+			"option":{
+				"clean_configs": [
+					{
+						"rules": [
+							{
+								"key": "headers.\"user-agent\"",
+								"value": [
+									"Google-Alerts"
+								],
+								"method": "eq",
+								"condition": "or"
+							},
+							{
+								"key": "__http_query_params__.source",
+								"value": [
+									"google"
+								],
+								"method": "eq",
+								"condition": "or"
+							}
+						],
+						"normalization_config": [
+							{
+								"field": "alert_name",
+								"expr": "incident.policy_name"
+							},
+							{
+								"field": "event_id",
+								"expr": "incident.incident_id"
+							},
+							{
+								"field": "description",
+								"expr": "incident.summary"
+							},
+							{
+								"field": "metric",
+								"expr": "incident.metric.displayName"
+							},
+							{
+								"field": "category",
+								"expr": "category"
+							},
+							{
+								"field": "assignee",
+								"expr": "assignee"
+							},
+							{
+								"field": "status",
+								"expr": "get_field({OPEN: 'ABNORMAL', open: 'ABNORMAL', CLOSED: 'CLOSED', closed: 'CLOSED'}, incident.state)"
+							},
+							{
+								"field": "target",
+								"expr": "target"
+							},
+							{
+								"field": "target_type",
+								"expr": "target_type"
+							},
+							{
+								"field": "severity",
+								"expr": "1"
+							},
+							{
+								"field": "bk_biz_id",
+								"expr": "bk_biz_id || '{{plugin_inst_biz_id}}'"
+							},
+							{
+								"field": "tags",
+								"expr": "{scoping_project_id: incident.scoping_project_id, scoping_project_number: incident.scoping_project_number, observed_value: condition.observed_value, resource: to_string(incident.resource), resource_id: incident.resource_id, resource_display_name: incident.resource_display_name, metric: to_string(incident.metric), metadata: to_string(incident.metadata), policy_user_labels: to_string(incident.policy_user_labels), condition: to_string(incident.condition)}"
+							},
+							{
+								"field": "time",
+								"expr": "incident.started_at"
+							},
+							{
+								"field": "anomaly_time",
+								"expr": "incident.started_at"
+							}
+						]
+					},
+					{
+						"rules": [
+							{
+								"key": "__http_query_params__.source",
+								"value": [
+									"tencent"
+								],
+								"method": "eq"
+							}
+						],
+						"normalization_config": [
+							{
+								"field": "alert_name",
+								"expr": "alarmPolicyInfo.policyName"
+							},
+							{
+								"field": "event_id",
+								"expr": "event_id"
+							},
+							{
+								"field": "description",
+								"expr": "alarmPolicyInfo.conditions.metricShowName && alarmPolicyInfo.conditions.calcType && alarmPolicyInfo.conditions.calcValue && alarmPolicyInfo.conditions.calcUnit && join(' ', [alarmPolicyInfo.conditions.metricShowName, alarmPolicyInfo.conditions.calcType, alarmPolicyInfo.conditions.calcValue, alarmPolicyInfo.conditions.calcUnit]) || alarmPolicyInfo.conditions.productShowName && alarmPolicyInfo.conditions.eventShowName && join(' ', [alarmPolicyInfo.conditions.productShowName, alarmPolicyInfo.conditions.eventShowName]) || alarmObjInfo.content"
+							},
+							{
+								"field": "metric",
+								"expr": "alarmPolicyInfo.conditions.metricName"
+							},
+							{
+								"field": "category",
+								"expr": "category"
+							},
+							{
+								"field": "assignee",
+								"expr": "assignee"
+							},
+							{
+								"field": "status",
+								"expr": "get_field({1: 'ABNORMAL', 0: 'RECOVERED'}, alarmStatus)"
+							},
+							{
+								"field": "target",
+								"expr": "target"
+							},
+							{
+								"field": "target_type",
+								"expr": "target_type"
+							},
+							{
+								"field": "severity",
+								"expr": "1"
+							},
+							{
+								"field": "bk_biz_id",
+								"expr": "bk_biz_id || '{{plugin_inst_biz_id}}'"
+							},
+							{
+								"field": "tags",
+								"expr": "{alarmObjInfo: to_string(alarmObjInfo), alarm_type: alarmType, policyId: alarmPolicyInfo.policyId}"
+							},
+							{
+								"field": "time",
+								"expr": "firstOccurTime"
+							},
+							{
+								"field": "source_time",
+								"expr": "firstOccurTime"
+							},
+							{
+								"field": "anomaly_time",
+								"expr": "firstOccurTime"
+							}
+						]
+					}
+				],
+				"alert_config": [],
+				"multiple_events":false,
+				"normalization_config":[]
+			},
+			"result_table_list":[
+				{
+					"schema_type":"free",
+					"shipper_list":[
+						{
+							"cluster_config":{
+								"domain_name":"kafka.service.consul",
+								"port":9092
+							},
+							"storage_config":{
+								"topic":"test_topic"
+							},
+							"cluster_type":"kafka"
+						}
+					],
+					"result_table":"base",
+					"field_list":[
+						{
+							"type": "string",
+							"is_config_by_user": true,
+							"field_name": "alert_name"
+						},
+						{
+							"type": "string",
+							"is_config_by_user": true,
+							"field_name": "event_id"
+						},
+						{
+							"type": "string",
+							"is_config_by_user": true,
+							"field_name": "target"
+						},
+						{
+							"type":"object",
+							"is_config_by_user": true,
+							"field_name":"tags"
+						},
+						{
+							"type":"string",
+							"is_config_by_user": true,
+							"field_name":"description"
+						},
+						{
+							"type":"int",
+							"is_config_by_user": true,
+							"field_name":"severity"
+						},
+						{
+							"type": "timestamp",
+							"is_config_by_user": true,
+							"field_name": "time",
+							"option": {
+								"time_format": "datetime",
+								"time_zone": 8
+							}
+						}
+					]
+				}
+			]
+		}`,
+	)
+
+	processor, err := fta.NewAlertFTAProcessor(s.CTX, "test")
+
+	s.NoError(err)
+	s.Run(`{"bk_data_id":1572956,"bk_plugin_id":"rest_api","bk_agent_id":"","ip":"127.0.0.1","hostname":"VM-68-183-centos","dataid":1572956,"bizid":0,"cloudid":0,"gseindex":36,"bk_host_id":0,"bk_ingest_time":1703843214,"bk_biz_id":2,"alarmType":"metric","alarmPolicyInfo":{"policyId":"policy-n4exeh88","policyType":"cvm_device","policyName":"test","policyTypeCName":"云服务器-基础监控","conditions":{"alarmNotifyType":"continuousAlarm","calcType":">","currentValue":"100","unit":"%","period":"60","historyValue":"5","periodNum":"1","alarmNotifyPeriod":300,"metricName":"cpu_usage","metricShowName":"CPU 利用率","calcValue":"90","calcUnit":"%"}},"durationTime":500,"recoverTime":"2017-03-09 07:50:00","__http_headers__":{"User-Agent":"curl/7.29.0","Accept":"*/*","Content-Type":"application/json","Content-Length":"1039","Expect":"100-continue"},"__http_query_params__":{"source":"tencent"},"sessionId":"xxxxxxxx","alarmStatus":"1","alarmObjInfo":{"region":"gz","namespace":"qce/cvm","appId":"xxxxxxxxxxxx","uin":"xxxxxxxxxxxx","dimensions":{"unInstanceId":"ins-o9p3rg3m","objId":"xxxxxxxxxxxx"}},"firstOccurTime":"2017-03-09 07:00:00","__bk_event_id__":"089c54d0-6331-4e8d-99bc-0f33bda0ecbd"}`,
+		processor,
+		func(result map[string]interface{}) {
+			delete(result, "bk_clean_time")
+			s.MapEqual(map[string]interface{}{
+				"alert_name":     "test",
+				"bk_ingest_time": float64(1703843214),
+				"description":    "CPU 利用率 > 90 %",
+				"event_id":       "089c54d0-6331-4e8d-99bc-0f33bda0ecbd",
+				"plugin_id":      "rest_api",
+				"target":         nil,
+				"tags": []interface{}{
+					map[string]interface{}{
+						"key":   "alarmObjInfo",
+						"value": `{"appId":"xxxxxxxxxxxx","dimensions":{"objId":"xxxxxxxxxxxx","unInstanceId":"ins-o9p3rg3m"},"namespace":"qce/cvm","region":"gz","uin":"xxxxxxxxxxxx"}`,
+					},
+					map[string]interface{}{
+						"key":   "alarm_type",
+						"value": "metric",
+					},
+					map[string]interface{}{
+						"key":   "policyId",
+						"value": "policy-n4exeh88",
+					},
+				},
 			}, result)
 		},
 	)

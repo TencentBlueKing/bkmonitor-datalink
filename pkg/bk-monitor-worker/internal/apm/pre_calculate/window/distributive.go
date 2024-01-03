60138143152510d37d846398e1af0637e2ca3586
rxwycdh <597701764@qq.com>
2024-01-03 12:39:49 +0800

    docs: 增加日志打印

--- pkg/bk-monitor-worker/internal/apm/pre_calculate/window/distributive.go
+++ pkg/bk-monitor-worker/internal/apm/pre_calculate/window/distributive.go
@@ -130,9 +130,9 @@ func NewDistributiveWindow(dataId string, ctx context.Context, processor Process
 
 func (w *DistributiveWindow) locate(uni string) *distributiveSubWindow {
 	hashValue := xxhash.Sum64([]byte(uni))
-	a := int(hashValue) % w.config.subWindowSize
-	w.logger.Infof("a --> %d windowLength: %d", a, len(w.subWindows))
-	return w.subWindows[a]
+	locationIndex := int(hashValue) % w.config.subWindowSize
+	w.logger.Infof("a --> %d windowLength: %d", locationIndex, len(w.subWindows))
+	return w.subWindows[locationIndex]
 }
 
 func (w *DistributiveWindow) Start(spanChan <-chan []StandardSpan, errorReceiveChan chan<- error, runtimeOpts ...RuntimeConfigOption) {

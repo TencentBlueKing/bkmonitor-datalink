121aa87333cf87010e23ac6a3845313a3eb5dea7
RyanYu <524860467@qq.com>
2024-01-18 09:48:46 +0800

    feat: add refresh_bcs_project_biz task

--- pkg/bk-monitor-worker/service/scheduler/periodic/periodic.go
+++ pkg/bk-monitor-worker/service/scheduler/periodic/periodic.go
@@ -41,6 +41,7 @@ var (
 	CleanExpiredRestore    = "periodic:metadata:clean_expired_restore"
 	RefreshESRestore       = "periodic:metadata:refresh_es_restore"
 	RefreshBcsMetricsLabel = "periodic:metadata:refresh_bcs_metrics_label"
+	RefreshBcsProjectBiz   = "periodic:metadata:refresh_bcs_project_biz"
 
 	ReportInfluxdbClusterMetrics = "periodic:cluster_metrics:report_influxdb"
 
@@ -96,6 +97,10 @@ var (
 		RefreshBcsMetricsLabel: {
 			Cron:    "*/10 * * * *",
 			Handler: metadataTask.RefreshBcsMetricsLabel,
+		},
+		RefreshBcsProjectBiz: {
+			Cron:    "*/10 * * * *",
+			Handler: metadataTask.RefreshBcsProjectBiz,
 		},
 		ReportInfluxdbClusterMetrics: {
 			Cron:    "*/1 * * * *",

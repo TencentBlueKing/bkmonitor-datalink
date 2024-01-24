cc07d7c68268215742723a208db88dc164a322f6
RyanYu <524860467@qq.com>
2024-01-24 16:02:54 +0800

    chore: bcs_cc替换成bcs_project

--- pkg/bk-monitor-worker/internal/api/bcsproject/bcs_project.go
+++ pkg/bk-monitor-worker/internal/api/bcsproject/bcs_project.go
@@ -7,21 +7,21 @@
 // an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
 // specific language governing permissions and limitations under the License.
 
-package bcs_cc
+package bcsproject
 
 import (
 	"github.com/TencentBlueKing/bk-apigateway-sdks/core/bkapi"
 	"github.com/TencentBlueKing/bk-apigateway-sdks/core/define"
 )
 
-// Client for bcs_cc
+// Client for bcs project
 type Client struct {
 	define.BkApiClient
 }
 
-// New bcs_cc client
+// New bcs_project client
 func New(configProvider define.ClientConfigProvider, opts ...define.BkApiClientOption) (*Client, error) {
-	client, err := bkapi.NewBkApiClient("bcs-cc", configProvider, opts...)
+	client, err := bkapi.NewBkApiClient("bcs-project", configProvider, opts...)
 	if err != nil {
 		return nil, err
 	}
@@ -29,12 +29,18 @@ func New(configProvider define.ClientConfigProvider, opts ...define.BkApiClientO
 	return &Client{BkApiClient: client}, nil
 }
 
-// GetProjects for bcs_cc resource get_projects
+// GetProjects for bcs project resource get_projects
+// 从bcs-project查询项目信息
 func (c *Client) GetProjects(opts ...define.OperationOption) define.Operation {
-	path := "projects"
+	/*
+		@params
+		limit 	| int 	 | 每页限制
+		offset  | int 	 | 数据偏移量
+		kind 	| string | 项目类型
+	*/
 	return c.BkApiClient.NewOperation(bkapi.OperationConfig{
 		Name:   "get projects",
 		Method: "GET",
-		Path:   path,
+		Path:   "projects",
 	}, opts...)
 }

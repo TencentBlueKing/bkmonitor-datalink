8203d660f0a2964678bba21e5e071e9374f38351
RyanYu <524860467@qq.com>
2024-02-22 17:54:17 +0800

    style: make imports

--- pkg/bk-monitor-worker/service/scheduler/async.go
+++ pkg/bk-monitor-worker/service/scheduler/async.go
@@ -20,12 +20,12 @@ type Task struct {
 }
 
 var (
-	exampleTask               = "async:test_example"
-	CreateEsStorageIndex      = "async:create_es_storage_index"
-	PushAndPublishSpaceRouter = "async:push_and_publish_space_router"
-	PushSpaceToRedis          = "async:push_space_to_redis"
-	AccessBkdataVm            = "async:access_bkdata_vm"
-	RefreshCustomReportConfig = "async:refresh_custom_report_config"
+	exampleTask                  = "async:test_example"
+	CreateEsStorageIndex         = "async:create_es_storage_index"
+	PushAndPublishSpaceRouter    = "async:push_and_publish_space_router"
+	PushSpaceToRedis             = "async:push_space_to_redis"
+	AccessBkdataVm               = "async:access_bkdata_vm"
+	RefreshCustomReportConfig    = "async:refresh_custom_report_config"
 	RefreshCustomLogReportConfig = "async:refresh_custom_log_report_config"
 
 	asyncTaskDefine = map[string]Task{

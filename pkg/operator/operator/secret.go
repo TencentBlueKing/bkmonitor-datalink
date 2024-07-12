0ea52425061f1767745dc8ecd45f504ead171d11
dongdong <chenjiandongx@qq.com>
2024-07-12 18:20:44 +0800

    feat: 修复 workers 调度异常问题 --story=118534514 (#439)

--- pkg/operator/operator/secret.go
+++ pkg/operator/operator/secret.go
@@ -392,6 +392,13 @@ func (c *Operator) createOrUpdateStatefulSetTaskSecrets(childConfigs []*discover
 		h := parseHost(config.Address)
 		w := workers[h]
 		mod := (w.Index + 1) % len(workers)
+
+		// n 为最初确定的 workers 数量
+		// 如果此时已经扩容了新节点 那先临时将其分配到 0 号 worker 上并等待下一个周期修正
+		if mod >= n {
+			mod = 0
+		}
+
 		groups[mod] = append(groups[mod], config)
 		logger.Infof("worker match antiaffinity rules, host=%s, worker%d(%s)", h, mod, indexWorkers[mod])
 

cc6b6ecd0191e2d4955e0044ebd5d318183fb9a5
chenjiandongx <chenjiandongx@qq.com>
2024-07-15 20:18:43 +0800

    chore: 修改构建流水线

--- pkg/bkmonitorbeat/script/gse_package.sh
+++ pkg/bkmonitorbeat/script/gse_package.sh
@@ -119,9 +119,9 @@ package() {
 
   # 构建带后缀的 tar
   find . -name 'bkmonitorbeat.conf.tpl' | xargs -I {} sh -c "sed -i '/enable_audit_tasks/d' {} && sed -i '/# --/,+2d' {}"
-  cd ${BUILD_PATH} && tar czf ${PACKAGE_NAME} * && cd -
-  cp ${BUILD_PATH}/${PACKAGE_NAME} ${DIST_PATH}/${PACKAGE_NAME}
-  cp ${BUILD_PATH}/${PACKAGE_NAME} ${DIST_PATH}/${MODULE_NAME}-${VER}-audit.tgz
+  cd ${BUILD_PATH} && tar czf ${PACKAGE_NAME}-audit * && cd -
+  cp ${BUILD_PATH}/${PACKAGE_NAME}-audit ${DIST_PATH}/${PACKAGE_NAME}-audit
+  cp ${BUILD_PATH}/${PACKAGE_NAME}-audit ${DIST_PATH}/${MODULE_NAME}-${VER}-audit.tgz
 
   echo "${LAST_COMMIT_ID}" > ${DIST_PATH}/${LAST_COMMIT_ID_FILE_NAME}
   rm -rf ${BUILD_PATH}

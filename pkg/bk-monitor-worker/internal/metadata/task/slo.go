dddaa2a30274eef0ee2dc23cf822fa65f4d8cb92
cuitong <1470501354@qq.com>
2024-08-28 20:30:33 +0800

    fix: 修改pre-commit

--- pkg/bk-monitor-worker/internal/metadata/task/slo.go
+++ pkg/bk-monitor-worker/internal/metadata/task/slo.go
@@ -1,18 +1,29 @@
+// Tencent is pleased to support the open source community by making
+// 蓝鲸智云 - 监控平台 (BlueKing - Monitor) available.
+// Copyright (C) 2022 THL A29 Limited, a Tencent company. All rights reserved.
+// Licensed under the MIT License (the "License"); you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at http://opensource.org/licenses/MIT
+// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+// an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+// specific language governing permissions and limitations under the License.
+
 package task
 
 import (
 	"context"
 	"fmt"
-	"github.com/TencentBlueKing/bkmonitor-datalink/pkg/bk-monitor-worker/internal/metadata/service"
-	"github.com/TencentBlueKing/bkmonitor-datalink/pkg/bk-monitor-worker/metrics"
-	"github.com/prometheus/client_golang/prometheus"
 	"sync"
 	"time"
 
-	t "github.com/TencentBlueKing/bkmonitor-datalink/pkg/bk-monitor-worker/task"
 	"github.com/TencentBlueKing/bkmonitor-datalink/pkg/utils/logger"
+	"github.com/prometheus/client_golang/prometheus"
+
+	"github.com/TencentBlueKing/bkmonitor-datalink/pkg/bk-monitor-worker/internal/metadata/service"
+	"github.com/TencentBlueKing/bkmonitor-datalink/pkg/bk-monitor-worker/metrics"
+	t "github.com/TencentBlueKing/bkmonitor-datalink/pkg/bk-monitor-worker/task"
 )
 
+// SloPush 上报slo数据指标
 func SloPush(ctx context.Context, t *t.Task) error {
 	defer func() {
 		if err := recover(); err != nil {
@@ -64,7 +75,8 @@ func SloPush(ctx context.Context, t *t.Task) error {
 				// 获取告警数据
 				AllStrategyAggInterval := service.GetAllAlertTime(TotalAlertTimeBucket, TrueSloName, bkBizID)
 				// 计算指标
-				service.CalculateMetric(TotalAlertTimeBucket, TrueSloName, AllStrategyAggInterval, TotalSloTimeBucketDict, bkBizID, scene)
+				service.CalculateMetric(TotalAlertTimeBucket, TrueSloName, AllStrategyAggInterval,
+					TotalSloTimeBucketDict, bkBizID, scene)
 			}
 		}()
 	}

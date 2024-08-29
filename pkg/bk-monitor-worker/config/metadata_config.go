57a384dfe2e927f5137644f82bc58154a23e6a70
cuitong <1470501354@qq.com>
2024-08-29 09:40:44 +0800

    feat: 增加自定义指标上报token

--- pkg/bk-monitor-worker/config/metadata_config.go
+++ pkg/bk-monitor-worker/config/metadata_config.go
@@ -123,6 +123,11 @@ var (
 	SkipInfluxdbTableIds []string
 	// 是否可以删除 consul 路径
 	CanDeleteConsulPath bool
+
+	// SloPushGatewayApi slo数据上报api
+	SloPushGatewayApi string
+	// SloPushGatewayToken slo数据上报Token
+	SloPushGatewayToken string
 )
 
 func initMetadataVariables() {
@@ -181,4 +186,7 @@ func initMetadataVariables() {
 	BkdataIsAllowAllCmdbLevel = GetValue("taskConfig.metadata.bkdata.isAllowAllCmdbLevel", false)
 	SkipInfluxdbTableIds = GetValue("taskConfig.metadata.global.skipInfluxdbTableIds", []string{})
 	CanDeleteConsulPath = GetValue("taskConfig.metadata.global.CanDeleteConsulPath", false)
+
+	SloPushGatewayApi = GetValue("taskConfig.metadata.slo.sloPushGatewayApi", BkApiUrl)
+	SloPushGatewayToken = GetValue("taskConfig.metadata.slo.sloPushGatewayToken", "")
 }
--- pkg/bk-monitor-worker/metrics/slometrics.go
+++ pkg/bk-monitor-worker/metrics/slometrics.go
@@ -1,11 +1,23 @@
+// Tencent is pleased to support the open source community by making
+// 蓝鲸智云 - 监控平台 (BlueKing - Monitor) available.
+// Copyright (C) 2022 THL A29 Limited, a Tencent company. All rights reserved.
+// Licensed under the MIT License (the "License"); you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at http://opensource.org/licenses/MIT
+// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+// an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+// specific language governing permissions and limitations under the License.
+
 package metrics
 
 import (
+	"log"
+	"net/http"
+
 	"github.com/TencentBlueKing/bkmonitor-datalink/pkg/utils/logger"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/push"
-	"log"
-	"net/http"
+
+	"github.com/TencentBlueKing/bkmonitor-datalink/pkg/bk-monitor-worker/config"
 )
 
 type bkClient struct{}
@@ -18,7 +30,7 @@ type MetricData struct {
 }
 
 func (c *bkClient) Do(r *http.Request) (*http.Response, error) {
-	r.Header.Set("X-BK-TOKEN", "Ymtia2JrYmtia2JrYmtiay8ZlFxKKayWx2yyrXrvGznuoRgwwyoWK99A0Q1VPSliq5+b9FDZ94IgIfceOC0nGQ==")
+	r.Header.Set("X-BK-TOKEN", config.SloPushGatewayToken)
 	return http.DefaultClient.Do(r)
 }
 

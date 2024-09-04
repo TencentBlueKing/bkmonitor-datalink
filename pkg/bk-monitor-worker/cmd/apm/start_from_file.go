98997478a89531ce2f12dbec0c3a97b958868348
rxwycdh <597701764@qq.com>
2024-09-04 19:17:01 +0800

    fix: 增加表格头

--- pkg/bk-monitor-worker/cmd/apm/start_from_file.go
+++ pkg/bk-monitor-worker/cmd/apm/start_from_file.go
@@ -1,3 +1,12 @@
+// Tencent is pleased to support the open source community by making
+// 蓝鲸智云 - 监控平台 (BlueKing - Monitor) available.
+// Copyright (C) 2022 THL A29 Limited, a Tencent company. All rights reserved.
+// Licensed under the MIT License (the "License"); you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at http://opensource.org/licenses/MIT
+// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+// an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+// specific language governing permissions and limitations under the License.
+
 package apm
 
 import (
@@ -49,7 +58,7 @@ func listenFile(cmd *cobra.Command, args []string) {
 
 	ctx, cancel := context.WithCancel(context.Background())
 	if err := tools.StartListenerFromFile(ctx, filePath); err != nil {
-		panic(err)
+		logger.Fatal(err)
 	}
 	s := make(chan os.Signal)
 	signal.Notify(s, syscall.SIGQUIT, syscall.SIGTERM, syscall.SIGINT)

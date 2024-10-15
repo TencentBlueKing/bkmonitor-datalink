5d4fc2f8f7bb71bc26721a71c54eb087a77bd065
chenjiandongx <chenjiandongx@qq.com>
2024-10-15 19:36:58 +0800

    feat: operator 支持 k8ssd 特性

--- pkg/operator/operator/promsd.go
+++ pkg/operator/operator/promsd.go
@@ -14,10 +14,13 @@ import (
 	"reflect"
 	"time"
 
+	"github.com/TencentBlueKing/bkmonitor-datalink/pkg/operator/operator/discover/kubernetesd"
 	"github.com/pkg/errors"
+	promv1 "github.com/prometheus-operator/prometheus-operator/pkg/apis/monitoring/v1"
 	"github.com/prometheus/common/model"
 	"github.com/prometheus/prometheus/config"
 	promhttpsd "github.com/prometheus/prometheus/discovery/http"
+	promk8ssd "github.com/prometheus/prometheus/discovery/kubernetes"
 	"gopkg.in/yaml.v2"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
@@ -171,6 +174,77 @@ func (c *Operator) createHttpSdDiscover(scrapeConfig config.ScrapeConfig, sdConf
 	return dis, nil
 }
 
+func (c *Operator) createKubernetesSdDiscover(scrapeConfig config.ScrapeConfig, sdConfig *promk8ssd.SDConfig, index int) (discover.Discover, error) {
+	metricRelabelings := make([]yaml.MapSlice, 0)
+	if len(scrapeConfig.MetricRelabelConfigs) != 0 {
+		for _, cfg := range scrapeConfig.MetricRelabelConfigs {
+			relabeling := generatePromRelabelConfig(cfg)
+			metricRelabelings = append(metricRelabelings, relabeling)
+		}
+	}
+
+	monitorMeta := define.MonitorMeta{
+		Name:      scrapeConfig.JobName,
+		Kind:      monitorKindKubernetesSd,
+		Namespace: "-", // 不标记 namespace
+		Index:     index,
+	}
+	// 默认使用 custommetric dataid
+	dataID, err := c.dw.MatchMetricDataID(monitorMeta, false)
+	if err != nil {
+		return nil, err
+	}
+
+	specLabels := dataID.Spec.Labels
+	httpClientConfig := scrapeConfig.HTTPClientConfig
+
+	var proxyURL string
+	if httpClientConfig.ProxyURL.URL != nil {
+		proxyURL = httpClientConfig.ProxyURL.String()
+	}
+
+	var bearerTokenFile string
+	auth := httpClientConfig.Authorization
+	if auth != nil && auth.Type == "Bearer" {
+		bearerTokenFile = auth.CredentialsFile
+	}
+
+	castDuration := func(d model.Duration) string {
+		if d <= 0 {
+			return ""
+		}
+		return d.String()
+	}
+
+	scrapeConfig.HTTPClientConfig.TLSConfig.CAFile
+
+	dis := kubernetesd.New(c.ctx, string(sdConfig.Role), c.objectsController.NodeNameExists, &kubernetesd.Options{
+		CommonOptions: &discover.CommonOptions{
+			MonitorMeta:            monitorMeta,
+			Name:                   monitorMeta.ID(),
+			DataID:                 dataID,
+			Relabels:               scrapeConfig.RelabelConfigs,
+			Path:                   scrapeConfig.MetricsPath,
+			Scheme:                 scrapeConfig.Scheme,
+			BearerTokenFile:        bearerTokenFile,
+			ProxyURL:               proxyURL,
+			Period:                 castDuration(scrapeConfig.ScrapeInterval),
+			Timeout:                castDuration(scrapeConfig.ScrapeTimeout),
+			DisableCustomTimestamp: !ifHonorTimestamps(&scrapeConfig.HonorTimestamps),
+			UrlValues:              scrapeConfig.Params,
+			ExtraLabels:            specLabels,
+			MetricRelabelConfigs:   metricRelabelings,
+		},
+		KubeConfig:       configs.G().KubeConfig,
+		Namespaces:       sdConfig.NamespaceDiscovery.Names,
+		Client:           c.client,
+		UseEndpointSlice: useEndpointslice,
+	})
+	logger.Infof("create httpsd discover: %s", dis.Name())
+
+	return dis, nil
+}
+
 func (c *Operator) createPromScrapeConfigDiscovers() []discover.Discover {
 	scrapeConfigs, ok := c.getPromScrapeConfigs()
 	if !ok {
@@ -190,6 +264,41 @@ func (c *Operator) createPromScrapeConfigDiscovers() []discover.Discover {
 					continue
 				}
 				discovers = append(discovers, httpSdDiscover)
+			case *promk8ssd.SDConfig:
+				//scrapeConfig.
+				dis := kubernetesd.New(c.ctx, string(obj.Role), c.objectsController.NodeNameExists, &kubernetesd.Options{
+					CommonOptions: &discover.CommonOptions{
+						MonitorMeta: monitorMeta,
+						//RelabelRule:            feature.RelabelRule(podMonitor.Annotations),
+						//RelabelIndex:           feature.RelabelIndex(podMonitor.Annotations),
+						//NormalizeMetricName:    feature.IfNormalizeMetricName(podMonitor.Annotations),
+						//AntiAffinity:           feature.IfAntiAffinity(podMonitor.Annotations),
+						//MatchSelector:          feature.MonitorMatchSelector(podMonitor.Annotations),
+						//DropSelector:           feature.MonitorDropSelector(podMonitor.Annotations),
+						//LabelJoinMatcher:       feature.LabelJoinMatcher(podMonitor.Annotations),
+						//ForwardLocalhost:       feature.IfForwardLocalhost(podMonitor.Annotations),
+						Name:                   monitorMeta.ID(),
+						DataID:                 dataID,
+						Relabels:               resultLabels,
+						Path:                   endpoint.Path,
+						Scheme:                 endpoint.Scheme,
+						Period:                 string(endpoint.Interval),
+						Timeout:                string(endpoint.ScrapeTimeout),
+						ProxyURL:               proxyURL,
+						ExtraLabels:            specLabels,
+						DisableCustomTimestamp: !ifHonorTimestamps(endpoint.HonorTimestamps),
+						System:                 systemResource,
+						UrlValues:              endpoint.Params,
+						MetricRelabelConfigs:   metricRelabelings,
+					},
+					Client:            c.client,
+					Namespaces:        namespaces,
+					KubeConfig:        configs.G().KubeConfig,
+					BasicAuth:         endpoint.BasicAuth.DeepCopy(),
+					BearerTokenSecret: endpoint.BearerTokenSecret.DeepCopy(),
+					TLSConfig:         &promv1.TLSConfig{SafeTLSConfig: safeTlsConfig},
+					UseEndpointSlice:  useEndpointslice,
+				})
 			}
 		}
 	}

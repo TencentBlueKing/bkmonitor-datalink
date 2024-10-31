3eab87d18c62ef81f3f834724b0e6bc159cc3f88
chenjiandongx <chenjiandongx@qq.com>
2024-10-31 16:53:05 +0800

    feat: operator 内存资源优化

--- pkg/operator/operator/discover/base.go
+++ pkg/operator/operator/discover/base.go
@@ -629,7 +629,7 @@ func (d *BaseDiscover) populateLabels(lset labels.Labels) (res, orig labels.Labe
 		}
 	}
 
-	preRelabelLabels := lb.Labels()
+	preRelabelLabels := lb.Labels(nil)
 	lset = relabel.Process(preRelabelLabels, d.opts.Relabels...)
 
 	// Check if the target was dropped.
@@ -686,7 +686,7 @@ func (d *BaseDiscover) populateLabels(lset labels.Labels) (res, orig labels.Labe
 		lb.Set(model.InstanceLabel, addr)
 	}
 
-	res = lb.Labels()
+	res = lb.Labels(nil)
 	for _, l := range res {
 		// Check label values are valid, drop the target if not.
 		if !model.LabelValue(l.Value).IsValid() {

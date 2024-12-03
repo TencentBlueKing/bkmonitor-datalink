af6227ac31a58ac4a3cfbad95ca91ef18854972f
shamcleren <shamcleren@tencent.com>
2024-12-03 21:54:19 +0800

    feat: 指标打点修改 #1010158081121034766

--- pkg/unify-query/service/http/middleware/jwt.go
+++ pkg/unify-query/service/http/middleware/jwt.go
@@ -143,10 +143,10 @@ func JwtAuthMiddleware(publicKey string, defaultAppCodeSpaces map[string][]strin
 			}
 
 			if err != nil {
-				err = fmt.Errorf("jwt auth unauthorized: %s, app_code: %s, space_uid: %s", err, appCode, spaceUID)
+				err = fmt.Errorf("jwt auth unauthorized: %s, app_code: %s, space_uid: %s", err, metricAppCode, spaceUID)
 				log.Errorf(ctx, err.Error())
 
-				metric.JWTRequestInc(ctx, userAgent, c.ClientIP(), c.Request.URL.Path, appCode, payLoad.UserName(), user.SpaceUid, metric.StatusFailed)
+				metric.JWTRequestInc(ctx, userAgent, c.ClientIP(), c.Request.URL.Path, metricAppCode, payLoad.UserName(), user.SpaceUid, metric.StatusFailed)
 
 				// 通过特性开关判断是否开启验证，如果未开启验证则不进行 504 校验，但是错误指标还正常处理
 				ffStatus := metadata.GetJwtAuthFeatureFlag(ctx)

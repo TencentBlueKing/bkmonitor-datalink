c74f7e54e744a4dcbce82205e253ba1a8ebc063c
shamcleren <shamcleren@tencent.com>
2025-02-26 11:48:06 +0800

    feat: 日志查询支持doris #1010158081121933504

--- pkg/unify-query/tsdb/bksql/sqlExpr/doris_test.go
+++ pkg/unify-query/tsdb/bksql/sqlExpr/doris_test.go
@@ -188,7 +188,7 @@ func TestDorisSQLExpr_ParserAllConditions(t *testing.T) {
 					{
 						DimensionName: "env",
 						Value:         []string{"prod", "test"},
-						Operator:      metadata.ConditionContains,
+						Operator:      metadata.ConditionEqual,
 					},
 				},
 			},

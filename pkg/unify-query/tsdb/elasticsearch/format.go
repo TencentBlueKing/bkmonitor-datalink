2328bf8708867716b99a423f59b4f8ab470d2395
shamcleren <shamcleren@tencent.com>
2025-04-01 14:46:53 +0800

    feat: es 聚合 missing 兼容 int 类型的字段 #1010158081122704186

--- pkg/unify-query/tsdb/elasticsearch/format.go
+++ pkg/unify-query/tsdb/elasticsearch/format.go
@@ -590,10 +590,8 @@ func (f *FormatFactory) Agg() (name string, agg elastic.Aggregation, err error) 
 			curName := info.Name
 			curAgg := elastic.NewTermsAggregation().Field(info.Name)
 			fieldType, ok := f.mapping[info.Name]
-			if ok {
-				if fieldType == Text || fieldType == KeyWord {
-					curAgg = curAgg.Missing(" ")
-				}
+			if !ok || fieldType == Text || fieldType == KeyWord {
+				curAgg = curAgg.Missing(" ")
 			}
 
 			if f.size > 0 {

3a82acc3fbf831c6f57edb0cce91348420d80bbb
chenjiandongx <chenjiandongx@qq.com>
2025-04-22 21:32:04 +0800

    chore: 细节优化

--- pkg/bkmonitorbeat/script/generate_support_files/write_root_conf_tpl.sh
+++ pkg/bkmonitorbeat/script/generate_support_files/write_root_conf_tpl.sh
@@ -255,16 +255,17 @@ fi
   # 时间同步服务采集
   timesync_task:
     dataid: 1100030
-    task_id: 120
+    task_id: 98
     period: 1m
     env: host
     query_timeout: 10s
     ntpd_path: /etc/ntp.conf
     chrony_address: "[::1]:323"
 
+  # dmesg 事件采集
   dmesg_task:
     dataid: 1100031
-    task_id: 121
+    task_id: 99
     period: 1m
 
 {%- if extra_vars is defined and extra_vars.enable_audit_tasks is defined and extra_vars.enable_audit_tasks == "true" %}

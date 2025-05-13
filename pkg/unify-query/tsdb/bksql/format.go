4b11fe2139f7f7d9d90cc6a88907525e48d6e53a
shamcleren <shamcleren@tencent.com>
2025-05-13 20:56:10 +0800

    feat: 全文检索支持前缀匹配 #1010158081123997480

--- pkg/unify-query/tsdb/bksql/format.go
+++ pkg/unify-query/tsdb/bksql/format.go
@@ -411,9 +411,9 @@ func (f *QueryFactory) BuildWhere() (string, error) {
 
 	// QueryString to sql
 	if f.query.QueryString != "" && f.query.QueryString != "*" {
-		qs, qsErr := f.expr.ParserQueryString(f.query.QueryString)
-		if qsErr != nil {
-			return "", qsErr
+		qs, err := f.expr.ParserQueryString(f.query.QueryString)
+		if err != nil {
+			return "", err
 		}
 
 		if qs != "" {

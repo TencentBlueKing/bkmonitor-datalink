76f6ac99910077941b07d858b4614317fa79bc74
Zhaoyikaiii <stryhytrgb@gmail.com>
2025-08-05 09:37:41 +0800

    feat: log warning for empty mappings in instance.go

--- pkg/unify-query/tsdb/elasticsearch/instance.go
+++ pkg/unify-query/tsdb/elasticsearch/instance.go
@@ -550,6 +550,9 @@ func (i *Instance) QueryRawData(ctx context.Context, query *metadata.Query, star
 				return
 			}
 			span.Set("mapping-length", len(mappings))
+			if len(mappings) == 0 {
+				log.Warnf(ctx, "index is empty with %v with %s", aliases, conn.String())
+			}
 
 			if i.maxSize > 0 && query.Size > i.maxSize {
 				query.Size = i.maxSize

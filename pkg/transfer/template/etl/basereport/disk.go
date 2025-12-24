c80cc17f588459d502d67ac42efc1a3776f8cddc
Kimmy <szk770235919@gmail.com>
2025-12-24 16:08:21 +0800

    fix:修复disk误加mountpoint的错误

--- pkg/transfer/template/etl/basereport/disk.go
+++ pkg/transfer/template/etl/basereport/disk.go
@@ -87,10 +87,6 @@ func NewPerformanceDiskProcessor(ctx context.Context, name string) *PerformanceD
 				etl.ExtractByJMESPath("usage.device"), etl.TransformNilString,
 			),
 			etl.NewSimpleField(
-				"mountpoint",
-				etl.ExtractByJMESPath("usage.mountpoint"), etl.TransformNilString,
-			),
-			etl.NewSimpleField(
 				"device_type",
 				etl.ExtractByJMESPath("usage.fstype"), etl.TransformNilString,
 			),

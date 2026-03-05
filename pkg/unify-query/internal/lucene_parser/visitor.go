8aaf690fcf5930480c2e34d0110e8218c34d1e41
gongzhaolong <1147108853@qq.com>
2026-03-06 04:01:40 +0800

    feat: 修复text字段通配符大小写敏感问题 #1010158081132157571

--- pkg/unify-query/internal/lucene_parser/visitor.go
+++ pkg/unify-query/internal/lucene_parser/visitor.go
@@ -581,6 +581,7 @@ func (n *ConditionNode) DSL() (allMust []elastic.Query, allShould []elastic.Quer
 				result = cq
 			}
 		} else {
+			// text 字段倒排索引为小写，wildcard 不经分词器，需手动小写化 pattern
 			if fieldOption.IsAnalyzed {
 				value = strings.ToLower(value)
 			}

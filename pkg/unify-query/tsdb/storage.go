80228b2f46b0b5056df7ca68cdebbd0bf0d7d044
gongzhaolong <1147108853@qq.com>
2026-04-09 21:34:45 +0800

    feat: 增加 storage 路由与内存一致性排查日志 #1010158081133389624

--- pkg/unify-query/tsdb/storage.go
+++ pkg/unify-query/tsdb/storage.go
@@ -16,21 +16,33 @@ import (
 
 	"github.com/TencentBlueKing/bkmonitor-datalink/pkg/unify-query/consul"
 	"github.com/TencentBlueKing/bkmonitor-datalink/pkg/unify-query/metadata"
+	"github.com/TencentBlueKing/bkmonitor-datalink/pkg/unify-query/trace"
 )
 
 var (
-	storageMap  = make(map[string]*Storage)
-	storageLock = new(sync.RWMutex)
+	storageMap     = make(map[string]*Storage)
+	storageMapHash string
+	storageLock    = new(sync.RWMutex)
 )
 
+// StorageMapHash 返回最近一次 ReloadTsDBStorage 写入的配置哈希（与 Consul 侧 hash 比对用于短路 reload）
+func StorageMapHash() string {
+	storageLock.RLock()
+	defer storageLock.RUnlock()
+	return storageMapHash
+}
+
 // ReloadTsDBStorage 重新加载存储实例到内存里面
-func ReloadTsDBStorage(_ context.Context, tsDBs map[string]*consul.Storage, opt *Options) error {
+func ReloadTsDBStorage(ctx context.Context, hash string, tsDBs map[string]*consul.Storage, opt *Options) error {
+	var err error
+	ctx, span := trace.NewSpan(ctx, "reload-tsdb-storage")
+	defer span.End(&err)
+
 	newStorageMap := make(map[string]*Storage, len(tsDBs))
+	oldHash := storageMapHash
 
 	for storageID, tsDB := range tsDBs {
-		var storage *Storage
-
-		storage = &Storage{
+		storage := &Storage{
 			Type:     tsDB.Type,
 			Address:  tsDB.Address,
 			Username: tsDB.Username,
@@ -57,12 +69,27 @@ func ReloadTsDBStorage(_ context.Context, tsDBs map[string]*consul.Storage, opt 
 			storage.AcceptEncoding = opt.InfluxDB.AcceptEncoding
 		}
 		newStorageMap[storageID] = storage
-
 	}
+
+	newKeys := make([]string, 0, len(newStorageMap))
+	for k := range newStorageMap {
+		newKeys = append(newKeys, k)
+	}
+
+	span.Set("old_hash", oldHash)
+	span.Set("new_hash", hash)
+	span.Set("storage_count", len(newStorageMap))
+	span.Set("storage_keys", fmt.Sprintf("%v", newKeys))
+
 	storageLock.Lock()
 	defer storageLock.Unlock()
 
 	storageMap = newStorageMap
+	storageMapHash = hash
+
+	metadata.NewMessage("tsdb_storage", "reload storage: old_hash=%s new_hash=%s count=%d keys=%v",
+		oldHash, hash, len(newStorageMap), newKeys).Info(ctx)
+
 	return nil
 }
 
@@ -77,12 +104,30 @@ func Print() string {
 }
 
 // GetStorage 初始化全局 tsdb 标准实例
-func GetStorage(storageID string) (*Storage, error) {
-	storageLock.Lock()
-	defer storageLock.Unlock()
+func GetStorage(ctx context.Context, storageID string) (*Storage, error) {
+	var err error
+	ctx, span := trace.NewSpan(ctx, "get-tsdb-storage")
+	defer span.End(&err)
+
+	storageLock.RLock()
+	defer storageLock.RUnlock()
+
+	keys := make([]string, 0, len(storageMap))
+	for k := range storageMap {
+		keys = append(keys, k)
+	}
+
+	span.Set("storage_id", storageID)
+	span.Set("storage_hash", storageMapHash)
+	span.Set("storage_keys", fmt.Sprintf("%v", keys))
+
+	metadata.NewMessage("tsdb_storage", "get storage: id=%s hash=%s map_keys=%v",
+		storageID, storageMapHash, keys).Info(ctx)
+
 	storage, ok := storageMap[storageID]
 	if !ok {
-		return nil, fmt.Errorf("%s: storageID: %s", ErrStorageNotFound, storageID)
+		err = fmt.Errorf("%s: storageID: %s", ErrStorageNotFound, storageID)
+		return nil, err
 	}
 	return storage, nil
 }
